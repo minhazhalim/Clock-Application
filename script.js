63810eb39ae8f2ba9d4911c2f1aa1404945062ea
Minhaz Halim <mhzim93@gmail.com>
2021-06-09 22:41:47 +0600

    first commit

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-setInterval(setClock,1000);
 const hourHand = document.querySelector('[data-hour-hand]');
 const minuteHand = document.querySelector('[data-minute-hand]');
 const secondHand = document.querySelector('[data-second-hand]');
@@ -11,7 +10,8 @@ function setClock(){
      setRotation(minuteHand,minutesRatio);
      setRotation(hourHand,hoursRatio);
 }
+setInterval(setClock,1000);
+setClock();
 function setRotation(element,rotationRatio){
      element.style.setProperty('--rotation',rotationRatio * 360);
 }
-setClock();
